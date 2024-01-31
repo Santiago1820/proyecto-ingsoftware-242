f7be10c63d2f8bb99c4b348d2fc28e4c919c2774
Santiago <csnegreteb@gmail.com>
2024-01-31 17:36:58 -0600

    Deply Project

---
+++ VentaMarketing/src/main/java/ventamarketing/VentaMarketing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ventamarketing;
+
+/**
+ *
+ * @author csneg
+ */
+public class VentaMarketing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!aa");
+    }
+}
