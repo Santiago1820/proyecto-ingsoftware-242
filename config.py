8cf1d6d81b1cbeca7273dbc5bac2687f2a85c40e
Santiago <csnegreteb@gmail.com>
2024-02-24 09:52:36 -0600

    comentamos controller

---
+++ config.py
@@ -0,0 +1,11 @@
+def nombre():
+    nombresys = "Sistema - Registro v1.0"
+    return nombresys
+
+def logo():
+    # Aqui vamos a ingresar cual sera nuestro logo
+    pass
+
+def icono():
+    # Aqui vamos a poner nuestro icono
+    pass
--- pages/controller.py
+++ pages/controller.py
@@ -2,51 +2,57 @@ import tkinter as Tk
 import pages.dash as dash, pages.register as register, pages.admin as admin
 import sys
 from pages.db import conexion
+from config import *
 
-nombresys = "Sistema - Registro v1.0"
+# Definimos las variables de configuración
+namesys = nombre()
+# Funcion para conectarnos a nuestra base de datos utilizando nuestro archivod e conexion bd.py
 conectar = conexion()
 cursor = conectar.cursor()
 
+# Funcion para cerrar la sesion de MySQL
 def cerrar_conexion():
     cursor.close()
     conectar.close()
 
+# Metodo para mostrarnuestra ventana de registro
 def show_register():
+    # Creamos nuestra nueva ventana
     new_root = Tk.Tk()
-    new_root.geometry("1200x750")
+    # Titulo de nuestra ventana
     new_root.title("Registrarse")
-    new_root.resizable(0,0)
+    # Cancelamos el evento cerrar
     new_root.protocol("WM_DELETE_WINDOW", no_cerrar)
+    # Mostramos la pagina en modo pantalla completa
     new_root.attributes('-fullscreen', True)
-    admin_page = register.registro(new_root)
-    admin_page.pack()
+    # Creamos nuestro registro y a imprimimos
+    register_page = register.Registro(new_root)
+    register_page.pack()
+    # Mostramos nuestro contenido
     new_root.mainloop()
 
 def show_dash():
     new_root = Tk.Tk()
-    new_root.geometry("1200x750")
-    new_root.resizable(0,0)
     new_root.protocol("WM_DELETE_WINDOW", no_cerrar)
     new_root.attributes('-fullscreen', True)
-    new_root.title(f"{nombresys}")
+    new_root.title(f"{namesys}")
     dash_page = dash.Dashboard(new_root)
     dash_page.pack()
     new_root.mainloop()
 
 def show_admin():
     new_root = Tk.Tk()
-    new_root.geometry("1200x750")
-    new_root.resizable(0,0)
     new_root.protocol("WM_DELETE_WINDOW", no_cerrar)
     new_root.attributes('-fullscreen', True)
-    new_root.title(f"{nombresys}")
+    new_root.title(f"{namesys}")
     admin_page = admin.Admin(new_root)
     admin_page.pack()
     new_root.mainloop()
 
+# Funcion para cerrar el sistema completo
 def cerrar_app():
     sys.exit()
 
-
+# Cancelamos el cerrar con un pass para pasar la accion
 def no_cerrar():
     pass
